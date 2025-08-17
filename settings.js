62d4e32a8419b680c03b49b14220b6890364d994
Juickidd-rgb <emmanueljrmatsambira@gmail.com>
2025-08-17 16:25:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-'Professor' settings = {
+'EmmanuelJrYourttings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'EmmanuelJrYour/ Your name
+  botOwner: 'JUICEKIDDYour/ Your name
   ownerNumber: '263788436159Set //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
